6fafeb2f9198f6a8a7eb1625e29705e7acb6d26f
Nadiia Palaida <n.palaida@gmail.com>
2023-03-22 21:43:56 +0200

    fix

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,14 +1,15 @@
 const menuBtns = document.querySelectorAll('.menu__mobile-btn')
+const menuItems = document.querySelectorAll('.menu__item')
 const header = document.getElementById('header')
 const menu = document.getElementById('menu')
 const body = document.getElementsByTagName('body')[0]
 
 let activeMobileMenu = false
 
-if(menuBtns.length) {
-    for(let i = 0; i < menuBtns.length; i++) {
+if (menuBtns.length) {
+    for (let i = 0; i < menuBtns.length; i++) {
         menuBtns[i].addEventListener('click', () => {
-            if(activeMobileMenu) {
+            if (activeMobileMenu) {
                 menu.classList.remove('active')
                 header.classList.remove('header-mobile-menu')
                 menuBtns[i].classList.remove('active')
@@ -21,5 +22,21 @@ if(menuBtns.length) {
             }
             activeMobileMenu = !activeMobileMenu
         })
+    }
+}
+
+if (menuItems.length) {
+    for (let i = 0; i < menuItems.length; i++) {
+        menuItems[i].addEventListener('click', () => {
+            console.log('click link')
+            unActiveAll()
+            menuItems[i].classList.add('active')
+        })
+    }
+
+    function unActiveAll() {
+        for (let i = 0; i < menuItems.length; i++) {
+            menuItems[i].classList.remove('active')
+        }
     }
 }
--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,8 +1,11 @@
 const modalRequest = document.querySelector('.modal-request')
 const modal = document.querySelectorAll('.modal')
+const modalWrap = document.querySelectorAll('.modal__wrap')
+// console.log('modalWrap', modalWrap)
 const body = document.getElementsByTagName('body')[0]
 const openBtns = document.querySelectorAll('.make-request-btn')
 const closeBtns = document.querySelectorAll('.modal__btn-close')
+const modalContents = document.querySelectorAll('.modal__content')
 
 const modalThanks = document.querySelector('.modal-thanks')
 const openThanksBtn = document.querySelector('.open-thanks-modal-btn')
@@ -20,6 +23,14 @@ if(closeBtns.length) {
     for(let i = 0; i < closeBtns.length; i++) {
         closeBtns[i].addEventListener('click', () => {
             closeModal()
+        })
+    }
+}
+
+if(modalContents.length) {
+    for(let i = 0; i < modalContents.length; i++) {
+        modalContents[i].addEventListener('click', (e) => {
+            e.stopPropagation()
         })
     }
 }
@@ -45,5 +56,27 @@ function closeModal() {
     }
     body.style.overflow = 'auto'
 }
+
+if(modalWrap.length) {
+    for(let i = 0; i < modalWrap.length; i++) {
+        modalWrap[i].addEventListener('click', () => {
+            console.log('close modal')
+            closeModal()
+        })
+    }
+}
+/*
+
+if(modalWrap) {
+    console.log('close modal')
+
+
+    modalWrap.addEventListener('click', () => {
+        console.log('close modal')
+        closeModal()
+    })
+}
+*/
+
 
 export {closeModal, openThanksModal}
